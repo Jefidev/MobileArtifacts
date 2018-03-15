9aa5f81bfdbbe60836043c836f39a01823a5a3c3
jefidev <jerome.fink@student.unamur.be>
2018-03-15 11:08:28 +0100

    Dependencies are ok now

--- API/src/main/scala/RESTServer/Main.scala
+++ API/src/main/scala/RESTServer/Main.scala
@@ -1,10 +1,24 @@
 package RESTServer
 
+import Main.api
+import com.twitter.finagle.Http
+import com.twitter.util.Await
+import io.finch.{Application, Endpoint, Ok}
+import io.finch.syntax.get
+
 /**
   * Created by jfink on 09/03/18.
   */
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello World")
+
+//https://finagle.github.io/blog/2014/12/10/rest-apis-with-finch/
+
+object Main extends App{
+
+  case class Payload(payload:String)
+
+  val bleh: Endpoint[Payload] = get("hello"){
+    Ok(Payload("Hello"))
   }
+
+  Await.ready(Http.server.serve(":8081", bleh.toService))
 }
