08c38f8bffc904eaa3b901784c91f05b89b065b8
jefidev <jerome.fink@student.unamur.be>
2018-05-08 14:27:56 +0200

    Modifying achievements

--- API/src/main/resources/db/migration/V2_2__Events.sql
+++ API/src/main/resources/db/migration/V2_2__Events.sql
@@ -23,12 +23,12 @@ create table Contexts(
 )
 ENGINE = InnoDB;
 
-insert into Events values (default, 0, 'Trouvez et scannez le signe du Beffroi', 1, 'KYT-1-789', null, 'Scan');
+insert into Events values (default, 0, 'Trouvez et scannez le signe de St Aubain', 1, 'KYT-1-789', null, 'Scan');
 
 insert into Events values (default, 1, 'Enregistrement au point de départ', 2, 'KYT-2-900', null, 'Enregistrement du départ');
 insert into Events values (default, 1, 'Enregistrement au point de destination', 2, 'KYT-3-900', 2, 'Validation du trajet');
 
-insert into Events values (default, 0, 'Scan du tag entre 16h00 et 18h00', 3, 'KYT-4-247', null, 'Scan');
-insert into Contexts values (4, 2, 100, 50);
+insert into Events values (default, 0, 'Scan d\'un tag au soleil couchant (16-18h)', 3, 'KYT-4-247', null, 'Scan');
+insert into Contexts values (4, 2, 300, 100);
 
 
