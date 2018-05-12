a104e32b6bad4264fc7e36e49a9c4b3a8ad36990
Oussama Essafi <oussama-essafi@hotmail.com>
2018-05-12 11:35:57 +0200

    I had to do it

--- API/src/main/scala/BusinessObjects/Orchestrators.scala
+++ API/src/main/scala/BusinessObjects/Orchestrators.scala
@@ -9,16 +9,23 @@ object Orchestrators {
 
   val repo = new SensorsRepository()
 
+  /**
+    * Check Raspberry Pi token
+    * @param token Raspberry Pi token
+    * @return true if the token is correct false otherwise
+    */
   def verify(token:String):Boolean = {
     println("Connexion raspberry reçue")
     println(token)
-    if(token == "269544d3-99e7-4c52-b1c4-660f4a59df32")
-      true
-    else
-      false
+    token == "269544d3-99e7-4c52-b1c4-660f4a59df32"
   }
 
-
+  /**
+    * Sets the token to be used for database queries
+    * @param token Raspberry Pi token
+    * @param id Sensor ID
+    * @param value Sensor value
+    */
   def setValue(token:String, id:Int, value:Float) = {
     repo.setValue(token, id, value)
   }
